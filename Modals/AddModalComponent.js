abf8303ce3be0fa942b8cefffc02ed452bea2f6f
HabteabT <habteabteame3@gmail.com>
2024-03-20 12:14:07 +0100

    fix a bug

--- Modals/AddModalComponent.js
+++ Modals/AddModalComponent.js
@@ -1,69 +1,45 @@
-import React, { useState } from "react";
-import {
-  Text,
-  Modal,
-  View,
-  TextInput,
-  KeyboardAvoidingView,
-  ScrollView,
-  Image,
-  Platform,
-} from "react-native";
+import React from "react";
+import { Modal, View, TextInput, KeyboardAvoidingView, ScrollView, Image, Platform } from "react-native";
 
-const AddModalComponent = ({
-  isModalVisible,
-  setModalVisible,
-  newEntry,
-  setNewEntry,
-  imageUri,
-}) => {
-  
-
+const AddModalComponent = ({ isModalVisible, setModalVisible, newEntry, setNewEntry, imageUri }) => {
   return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={isModalVisible}
-      onRequestClose={() => setModalVisible(false)}
-    >
-      <View style={styles.modalContainer}>
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"git 
-        >
-          <View style={styles.modalButtonsAdd}>
- 
-          </View>
-
-          <ScrollView
-            style={styles.scrollContainer}
-            contentContainerStyle={styles.scrollContentContainer}
-            showsVerticalScrollIndicator={false}
-          >
-            <ScrollView style={styles.inputContainer}>
-              <View style={styles.selectedInfoContainer3}>
-                {imageUri && (
-                  <Image
-                    source={{ uri: imageUri }}
-                    style={styles.uploadedImage}
-                    resizeMode="cover"
-                  />
-                )}
-
-              </View>
-
-              <TextInput
-                style={styles.input}
-                placeholder="Add a new diary entry"
-                value={newEntry}
-                onChangeText={(text) => setNewEntry(text)}
-                multiline
-                numberOfLines={1}
-              />
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={isModalVisible}
+          onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <View style={styles.modalButtonsAdd}></View>
+            <ScrollView
+                style={styles.scrollContainer}
+                contentContainerStyle={styles.scrollContentContainer}
+                showsVerticalScrollIndicator={false}
+            >
+              <ScrollView style={styles.inputContainer}>
+                <View style={styles.selectedInfoContainer3}>
+                  {imageUri && (
+                      <Image
+                          source={{ uri: imageUri }}
+                          style={styles.uploadedImage}
+                          resizeMode="cover"
+                      />
+                  )}
+                </View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Add a new diary entry"
+                    value={newEntry}
+                    onChangeText={(text) => setNewEntry(text)}
+                    multiline
+                    numberOfLines={1}
+                />
+              </ScrollView>
             </ScrollView>
-          </ScrollView>
-        </KeyboardAvoidingView>
-      </View>
-    </Modal>
+          </KeyboardAvoidingView>
+        </View>
+      </Modal>
   );
 };
 
