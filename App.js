472d90030f462ea5bc499668bc1d0f3614772834
amanuelHT <aman3hda@gmail.com>
2024-03-21 12:51:12 +0100

    Dia10 adedd tabNavigation and App.js

--- App.js
+++ App.js
@@ -1,16 +1,70 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, ImageBackground } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./navigation/TabNavigator";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
 import Diary from "./Screens/DiaryScreen";
 import HomeScreen from "./Screens/HomeScreen";
-import LoginScreen from "./Screens/LoginScreen";
-import SignUpScreen from "./Screens/SignUpScreen";
 import ProfileScreen from "./Screens/ProfileScreen";
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <ImageBackground
+      source={require("./images/background1.jpg")}
+      style={styles.backgroundImage}
+    >
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Tabs"
+          screenOptions={{
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+            },
+          }}
+        >
+          <Stack.Screen
+            name="Tabs"
+            component={TabNavigator}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignUpScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+});
+
+
+
+/*const App = () => {
   return (
     <View style={styles.container}>
-      <Diary />
+      <ProfileScreen />
     </View>
   );
 };
@@ -45,3 +99,5 @@ const styles = StyleSheet.create({
   },
 });
 ----------*/
+
+
