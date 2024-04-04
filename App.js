840207ab2409723917acd573871d0353ea3acd38
teklit amanuel <teklitag@uia.no>
2024-04-04 13:05:27 +0200

    fix merge conflic

--- App.js
+++ App.js
@@ -5,13 +5,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigator from "./navigation/TabNavigator";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from "react";
-import { View, StyleSheet } from "react-native";
-import Diary from "./Screens/DiaryScreen";
-import HomeScreen from "./Screens/HomeScreen";
-import ProfileScreen from "./Screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -58,46 +51,3 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
 });
-
-
-
-/*const App = () => {
-  return (
-    <View style={styles.container}>
-      <ProfileScreen />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-export default App;
-
-/*---------
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-----------*/
-
-
