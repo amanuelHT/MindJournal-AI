2ac2a65ba6c695f0ab9c4cfaf55259b3acd1c0c3
HabteabT <habteabteame3@gmail.com>
2024-04-04 13:37:05 +0200

    fix app.js

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import Diary from "./Screens/DiaryScreen";
-import HomeScreen from "./Screens/HomeScreen";
-import ProfileScreen from "./Screens/ProfileScreen";
+import Diary from "./screens/DiaryScreen";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -58,46 +58,5 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
 });
-
-
-
-/*const App = () => {
-  return (
-    <View style={styles.container}>
-      <ProfileScreen />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-export default App;
-
-/*---------
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-----------*/
 
 
